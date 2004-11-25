81584abcbf445164e3b034463843c102a3d0724c
laszlo <laszlo@ce359eda-5150-e5dd-abfe-a9d5ac1c7f9b>
2004-11-25 14:41:08 +0000

    first demo added
    git-svn-id: https://svn.java.net/svn/jdic~svn/trunk@551 ce359eda-5150-e5dd-abfe-a9d5ac1c7f9b

---
+++ src/incubator/floating-dock/demo/Dock/DockDemo.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2004 Sun Microsystems, Inc. All rights reserved. Use is
+ * subject to license terms.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA.
+ */
+
+import org.jdesktop.jdic.dock.FloatingDock;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Dimension;
+
+class DockDemo {
+
+    private static void createTestGUI() {
+        //Make sure we have nice window decorations.
+        JFrame.setDefaultLookAndFeelDecorated(true);
+
+        //Create and set up the window.
+        JFrame frame = new JFrame("HelloWorldSwing");
+        //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Add the ubiquitous "Hello World" label.
+        JLabel label = new JLabel("Hello World");
+        frame.getContentPane().add(label);
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+
+    public static void main(String[] args) {
+	FloatingDock fd = new FloatingDock();
+
+	fd.setSize (new Dimension (100, 100));
+    }
+}
