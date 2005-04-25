3f53fc95f4fe6b5372467ce1987fcc8b3d7361ef
carldea <carldea@ce359eda-5150-e5dd-abfe-a9d5ac1c7f9b>
2005-04-25 04:03:39 +0000

    Makefile replaced this file for Mingw's make.
    git-svn-id: https://svn.java.net/svn/jdic~svn/trunk@944 ce359eda-5150-e5dd-abfe-a9d5ac1c7f9b

--- src/incubator/misc/src/win32/native/jni/makeit.bat
+++
@@ -1,6 +0,0 @@
-gcc -g -O2 -c -Ic:/j2sdk1.4.2_07/include -Ic:/j2sdk1.4.2_07/include/win32 -g wallpaper.cpp
-dllwrap --driver-name=c++ --add-stdcall-alias -o jdic_misc.dll -s wallpaper.o
-
-cp jdic_misc.dll c:/JDIC_DEV/jdic/src/incubator/misc/demo
-cp jdic_misc.dll c:/JDIC_DEV/jdic/src/incubator/misc/dist/windows
-
