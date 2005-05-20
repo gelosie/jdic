d4ff29f03b9a0821469f4f53cd2abfda3013303b
armin_chen <armin_chen@ce359eda-5150-e5dd-abfe-a9d5ac1c7f9b>
2005-05-20 07:35:24 +0000

    no message
    git-svn-id: https://svn.java.net/svn/jdic~svn/trunk@1026 ce359eda-5150-e5dd-abfe-a9d5ac1c7f9b

--- src/jdic/src/win32/native/mozilla/MozEmbed.cpp
+++ src/jdic/src/win32/native/mozilla/MozEmbed.cpp
@@ -686,8 +686,8 @@ void MozEmbedApp::MessageReceived(const char * msg)
     case JEVENT_DESTROYWINDOW:
         if( m_FrameWndArray[instance] != NULL){
             ((CBrowserFrame *)m_FrameWndArray[instance])->DestroyBrowserFrame();
-            m_FrameWndArray[instance].SetAt(instance, NULL);
-		}
+            m_FrameWndArray.SetAt(instance, NULL);
+        }
         SendSocketMessage(instance, CEVENT_DISTORYWINDOW_SUCC);
         break;
     case JEVENT_SHUTDOWN:
