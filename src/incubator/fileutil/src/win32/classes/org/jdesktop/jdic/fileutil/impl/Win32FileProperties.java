79fd596f79538be3329f43ceb5a4f3073f1fe940
fabiocastilhomartins <fabiocastilhomartins@ce359eda-5150-e5dd-abfe-a9d5ac1c7f9b>
2005-05-27 11:48:25 +0000

    *** empty log message ***
    git-svn-id: https://svn.java.net/svn/jdic~svn/trunk@1044 ce359eda-5150-e5dd-abfe-a9d5ac1c7f9b

--- src/incubator/fileutil/src/win32/classes/org/jdesktop/jdic/fileutil/impl/Win32FileProperties.java
+++ src/incubator/fileutil/src/win32/classes/org/jdesktop/jdic/fileutil/impl/Win32FileProperties.java
@@ -26,55 +26,111 @@ import java.io.IOException;
 import org.jdesktop.jdic.fileutil.FileProperties;
 
 /**
+ * Windows specific File properties.
+ * 
  * @author Fábio Castilho Martins
- *  
+ *
  */
 public class Win32FileProperties implements FileProperties {
 	
+	/**
+	 * Returns true if the file/directory is marked for backup/removal.
+	 * @throws IOException
+	*/
 	public boolean isArchive(File file) throws IOException {
 		return Win32NativeFileUtil.isArchive(file.getCanonicalPath());		
 	}
 	
+	/**
+	 * Returns true if the file/directory is marked normal (no other properties set).
+	 * @throws IOException
+	*/
 	public boolean isNormal(File file) throws IOException {
 		return Win32NativeFileUtil.isNormal(file.getCanonicalPath());		
 	}
 	
+	/**
+	 * Returns true if the file/directory is marked read only. For a file, 
+	 * it can't be wrote or deleted, for a directory, it can't be deleted.
+	 * @throws IOException
+	*/
 	public boolean isReadOnly(File file) throws IOException {
 		return Win32NativeFileUtil.isReadOnly(file.getCanonicalPath());
 	}
 	
+	/**
+	 * Returns true if the file/directory is used by the operating system.
+	 * @throws IOException
+	*/
 	public boolean isSystem(File file) throws IOException {
 		return Win32NativeFileUtil.isSystem(file.getCanonicalPath());
 	}
 	
+	/**
+	 * Returns true if the file/directory is temporary.
+	 * @throws IOException
+	*/
 	public boolean isTemporary(File file) throws IOException {
 		return Win32NativeFileUtil.isTemporary(file.getCanonicalPath());
 	}
 	
+	/**
+	 * Returns true if the file/directory data is compressed.
+	 * @throws IOException
+	*/
 	public boolean isCompressed(File file) throws IOException {
 		return Win32NativeFileUtil.isCompressed(file.getCanonicalPath());
 	}
 	
+	/**
+	 * Returns true if the file/directory data is encrypted.
+	 * @throws IOException
+	*/
 	public boolean isEncrypted(File file) throws IOException {
 		return Win32NativeFileUtil.isEncrypted(file.getCanonicalPath());
 	}
 	
+	/**
+	 * Marks the file/directory for backup/removal.
+	 * @return true if successful, false otherwise.
+	 * @throws IOException
+	*/
 	public boolean setArchive(File file) throws IOException {
 		return Win32NativeFileUtil.setArchive(file.getCanonicalPath());		
 	}
 	
+	/**
+	 * Sets all other file/directory properties (except compressed and encrypted) false.
+	 * @return true if successful, false otherwise.
+	 * @throws IOException
+	*/
 	public boolean setNormal(File file) throws IOException {
 		return Win32NativeFileUtil.setNormal(file.getCanonicalPath());		
 	}
 	
+	/**
+	 * Marks the file/directory for use by the operating system.
+	 * @return true if successful, false otherwise.
+	 * @throws IOException
+	*/
 	public boolean setSystem(File file) throws IOException {
 		return Win32NativeFileUtil.setSystem(file.getCanonicalPath());
 	}
 	
+	/**
+	 * Marks the file/directory for temporary use.
+	 * @return true if successful, false otherwise.
+	 * @throws IOException
+	*/
 	public boolean setTemporary(File file) throws IOException {
 		return Win32NativeFileUtil.setTemporary(file.getCanonicalPath());
 	}
 	
+	/**
+	 * Hides the file/directory. The file/directory will no be seen in an directory listing.
+	 * @return true if successful, false otherwise.
+	 * @throws IOException
+	*/
 	public boolean setHidden(File file) throws IOException {
 		return Win32NativeFileUtil.setHidden(file.getCanonicalPath());
 	}
