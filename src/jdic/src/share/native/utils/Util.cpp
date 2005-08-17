6fb52de8beb4d8df266770c5be0067806b3c523a
georgez <georgez@ce359eda-5150-e5dd-abfe-a9d5ac1c7f9b>
2005-08-17 02:56:30 +0000

    Code refactoring to replace tabs with 4-spaces.
    git-svn-id: https://svn.java.net/svn/jdic~svn/trunk@1218 ce359eda-5150-e5dd-abfe-a9d5ac1c7f9b

--- src/jdic/src/share/native/utils/Util.cpp
+++ src/jdic/src/share/native/utils/Util.cpp
@@ -45,76 +45,76 @@ void WBTrace(const char *format, ...)
 ///////////////////////////////////////////////////////////
 WBArray::WBArray()
 {
-	m_pData = NULL;
-	m_nSize = m_nMaxSize = 0;
+    m_pData = NULL;
+    m_nSize = m_nMaxSize = 0;
 }
 
 WBArray::~WBArray()
 {
-	delete[] (char*)m_pData;
+    delete[] (char*)m_pData;
 }
 
 void WBArray::SetSize(int nNewSize)
 {
-	if (nNewSize == 0)
-	{
-		// shrink to nothing
-		delete[] (char*)m_pData;
-		m_pData = NULL;
-		m_nSize = m_nMaxSize = 0;
-	}
-	else if (m_pData == NULL)
-	{
-		// create one with exact size
-		m_pData = (void**) new char[nNewSize * sizeof(void*)];
+    if (nNewSize == 0)
+    {
+        // shrink to nothing
+        delete[] (char*)m_pData;
+        m_pData = NULL;
+        m_nSize = m_nMaxSize = 0;
+    }
+    else if (m_pData == NULL)
+    {
+        // create one with exact size
+        m_pData = (void**) new char[nNewSize * sizeof(void*)];
 
-		memset(m_pData, 0, nNewSize * sizeof(void*));  // zero fill
+        memset(m_pData, 0, nNewSize * sizeof(void*));  // zero fill
 
-		m_nSize = m_nMaxSize = nNewSize;
-	}
-	else if (nNewSize <= m_nMaxSize)
-	{
-		// it fits
-		if (nNewSize > m_nSize)
-		{
-			// initialize the new elements
-			memset(&m_pData[m_nSize], 0, (nNewSize-m_nSize) * sizeof(void*));
-		}
+        m_nSize = m_nMaxSize = nNewSize;
+    }
+    else if (nNewSize <= m_nMaxSize)
+    {
+        // it fits
+        if (nNewSize > m_nSize)
+        {
+            // initialize the new elements
+            memset(&m_pData[m_nSize], 0, (nNewSize-m_nSize) * sizeof(void*));
+        }
 
-		m_nSize = nNewSize;
-	}
-	else
-	{
-		// otherwise, grow array
-		int nGrowBy = min(1024, max(4, m_nSize / 8));
-		
+        m_nSize = nNewSize;
+    }
+    else
+    {
+        // otherwise, grow array
+        int nGrowBy = min(1024, max(4, m_nSize / 8));
+        
         int nNewMax;
-		if (nNewSize < m_nMaxSize + nGrowBy)
-			nNewMax = m_nMaxSize + nGrowBy;  // granularity
-		else
-			nNewMax = nNewSize;  // no slush
+        if (nNewSize < m_nMaxSize + nGrowBy)
+            nNewMax = m_nMaxSize + nGrowBy;  // granularity
+        else
+            nNewMax = nNewSize;  // no slush
 
-		void** pNewData = (void**) new char[nNewMax * sizeof(void*)];
+        void** pNewData = (void**) new char[nNewMax * sizeof(void*)];
 
-		// copy new data from old
-		memcpy(pNewData, m_pData, m_nSize * sizeof(void*));
-		memset(&pNewData[m_nSize], 0, (nNewSize-m_nSize) * sizeof(void*));
+        // copy new data from old
+        memcpy(pNewData, m_pData, m_nSize * sizeof(void*));
+        memset(&pNewData[m_nSize], 0, (nNewSize-m_nSize) * sizeof(void*));
 
-		// get rid of old stuff (note: no destructors called)
-		delete[] (char*)m_pData;
-		m_pData = pNewData;
-		m_nSize = nNewSize;
-		m_nMaxSize = nNewMax;
-	}
+        // get rid of old stuff (note: no destructors called)
+        delete[] (char*)m_pData;
+        m_pData = pNewData;
+        m_nSize = nNewSize;
+        m_nMaxSize = nNewMax;
+    }
 }
 
 /////////////////////////////////////////////////////////////////////////////
 
 void WBArray::SetAtGrow(int nIndex, void* newElement)
 {
-	if (nIndex >= m_nSize)
-		SetSize(nIndex+1);
-	m_pData[nIndex] = newElement;
+    if (nIndex >= m_nSize)
+        SetSize(nIndex+1);
+    m_pData[nIndex] = newElement;
 }
 
 int WBArray::Add(void* newElement)
@@ -126,38 +126,38 @@ int WBArray::Add(void* newElement)
 
 void WBArray::InsertAt(int nIndex, void* newElement, int nCount)
 {
-	if (nIndex >= m_nSize)
-	{
-		// adding after the end of the array
-		SetSize(nIndex + nCount);  // grow so nIndex is valid
-	}
-	else
-	{
-		// inserting in the middle of the array
-		int nOldSize = m_nSize;
-		SetSize(m_nSize + nCount);  // grow it to new size
-		// shift old data up to fill gap
-		memmove(&m_pData[nIndex+nCount], &m_pData[nIndex],
-			(nOldSize-nIndex) * sizeof(void*));
+    if (nIndex >= m_nSize)
+    {
+        // adding after the end of the array
+        SetSize(nIndex + nCount);  // grow so nIndex is valid
+    }
+    else
+    {
+        // inserting in the middle of the array
+        int nOldSize = m_nSize;
+        SetSize(m_nSize + nCount);  // grow it to new size
+        // shift old data up to fill gap
+        memmove(&m_pData[nIndex+nCount], &m_pData[nIndex],
+            (nOldSize-nIndex) * sizeof(void*));
 
-		// re-init slots we copied from
-		memset(&m_pData[nIndex], 0, nCount * sizeof(void*));
-	}
+        // re-init slots we copied from
+        memset(&m_pData[nIndex], 0, nCount * sizeof(void*));
+    }
 
-	// copy elements into the empty space
-	while (nCount--)
-		m_pData[nIndex++] = newElement;
+    // copy elements into the empty space
+    while (nCount--)
+        m_pData[nIndex++] = newElement;
 }
 
 void WBArray::RemoveAt(int nIndex, int nCount)
 {
-	// just remove a range
-	int nMoveCount = m_nSize - (nIndex + nCount);
+    // just remove a range
+    int nMoveCount = m_nSize - (nIndex + nCount);
 
-	if (nMoveCount)
-		memmove(&m_pData[nIndex], &m_pData[nIndex + nCount],
-			nMoveCount * sizeof(void*));
-	m_nSize -= nCount;
+    if (nMoveCount)
+        memmove(&m_pData[nIndex], &m_pData[nIndex + nCount],
+            nMoveCount * sizeof(void*));
+    m_nSize -= nCount;
 }
 
 /////////////////////////////////////////////////////////////////////////////
