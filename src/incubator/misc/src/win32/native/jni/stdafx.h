9070aba948eadecf3c259d2c2e8aebc912248f98
carldea <carldea@ce359eda-5150-e5dd-abfe-a9d5ac1c7f9b>
2005-08-17 05:59:09 +0000

    MFC Windows includes
    git-svn-id: https://svn.java.net/svn/jdic~svn/trunk@1220 ce359eda-5150-e5dd-abfe-a9d5ac1c7f9b

---
+++ src/incubator/misc/src/win32/native/jni/stdafx.h
@@ -0,0 +1,25 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+
+#pragma once
+
+#ifndef VC_EXTRALEAN
+#define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
+#endif
+
+// Modify the following defines if you have to target a platform prior to the ones specified below.
+// Refer to MSDN for the latest info on corresponding values for different platforms.
+#ifndef WINVER				// Allow use of features specific to Windows 95 and Windows NT 4 or later.
+#define WINVER 0x0400		// Change this to the appropriate value to target Windows 98 and Windows 2000 or later.
+#endif
+
+#ifndef _WIN32_IE			// Allow use of features specific to IE 4.0 or later.
+#define _WIN32_IE 0x0400	// Change this to the appropriate value to target IE 5.0 or later.
+#endif
+
+
+#include <afxwin.h>         // MFC core and standard components
+#include <wininet.h>
+#include <shlobj.h>
+
