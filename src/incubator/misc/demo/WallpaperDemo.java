3bfc3636dcafb4470148f48352983c0d5f65f51f
carldea <carldea@ce359eda-5150-e5dd-abfe-a9d5ac1c7f9b>
2005-08-17 06:55:00 +0000

    supporting the styles such as stretch, center, and tile.
    git-svn-id: https://svn.java.net/svn/jdic~svn/trunk@1222 ce359eda-5150-e5dd-abfe-a9d5ac1c7f9b

--- src/incubator/misc/demo/WallpaperDemo.java
+++ src/incubator/misc/demo/WallpaperDemo.java
@@ -23,11 +23,10 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-
 import java.beans.PropertyChangeEvent;
-
 import java.io.File;
 
 import javax.swing.BorderFactory;
@@ -62,7 +61,10 @@ extends JPanel
     JFileChooser jFileChooser = new JFileChooser();
     JPanel jStylePanel = new JPanel();
     JLabel jStyleLabel = new JLabel("Style:");
-    String[] wallpaperStyles = { "Stretch    ", "Center  ", "Tile  " };
+    Object[] wallpaperStyles = { new WallpaperMode("Stretch", "This will stretch the picture", Wallpaper.STRETCH), 
+                                 new WallpaperMode("Center", "This will center the picture", Wallpaper.CENTER), 
+                                 new WallpaperMode("Tile", "This will tile the picture", Wallpaper.TILE)
+                                 };
     JComboBox jStyleComboBox = new JComboBox(wallpaperStyles);
     JPanel jBottomPanel = new JPanel();
     JPanel jButtonPanel = new JPanel();
@@ -127,6 +129,11 @@ extends JPanel
         jStylePanel.setBorder(BorderFactory.createEmptyBorder(44, 10, 0, 10));
         jStylePanel.add(jStyleLabel, null);
         jStylePanel.add(jStyleComboBox, null);
+        jStyleComboBox.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                jSetWallpaperButton.setActionCommand(jStyleComboBox.getSelectedItem().toString());
+            }
+        });
 
         fileChooserPanel.setLayout(new BorderLayout());
         fileChooserPanel.setBorder(BorderFactory.createEmptyBorder(0, 0, 10, 0));
@@ -166,10 +173,23 @@ extends JPanel
 
     void jSetWallpaperButton_actionPerformed(ActionEvent e)
     {
-        // TO DO: 
         // below code needs to be updated to handle the selected style.
         Wallpaper wallpaper = WallpaperFactory.createWallpaper();
-        wallpaper.setBackground(selectedFile.getAbsolutePath(), 1);
+        int mode = 0;
+        if (e.getActionCommand().equalsIgnoreCase("stretch"))
+        {
+           mode = Wallpaper.STRETCH;
+        }
+        else if (e.getActionCommand().equalsIgnoreCase("center"))
+        {
+           mode = Wallpaper.CENTER;
+        }
+        else if (e.getActionCommand().equalsIgnoreCase("tile"))
+        {
+           mode = Wallpaper.TILE;
+        }
+        
+        wallpaper.setBackground(selectedFile.getAbsolutePath(), mode);
     }
 
 
@@ -287,5 +307,54 @@ extends FileFilter
     public String getDescription()
     {
         return "Background Files (*.bmp;*.gif;*.jpg;*.jpeg;*.dib;*.png)";
+    }
+}
+
+class WallpaperMode 
+{
+
+    String _displayName;
+    String _description;
+    int _modeId;
+
+    public WallpaperMode ()
+    {
+     
+    }
+    public WallpaperMode (String name, String desc, int mode)
+    {
+        setDisplayName(name);
+        setDescription(desc);
+        setModeId(mode);
+    }
+    public String getDescription() 
+    {
+        return _description;
+    }
+    public void setDescription(String description) 
+    {
+        _description = description;
+    }
+    
+    public String getDisplayName() 
+    {
+        return _displayName;
+    }
+    public void setDisplayName(String displayName) 
+    {
+        _displayName = displayName;
+    }
+    public int getModeId() 
+    {
+        return _modeId;
+    }
+    public void setModeId(int modeId) 
+    {
+        _modeId = modeId;
+    }
+    
+    public String toString() 
+    {
+        return getDisplayName();
     }
 }
