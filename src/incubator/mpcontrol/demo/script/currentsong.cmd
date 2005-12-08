13b95471418c8c1b329e6ec1b0cd1b3fcf77681c
zombi <zombi@ce359eda-5150-e5dd-abfe-a9d5ac1c7f9b>
2005-12-08 01:21:28 +0000

    the jar name fixed.
    git-svn-id: https://svn.java.net/svn/jdic~svn/trunk@1283 ce359eda-5150-e5dd-abfe-a9d5ac1c7f9b

--- src/incubator/mpcontrol/demo/script/currentsong.cmd
+++ src/incubator/mpcontrol/demo/script/currentsong.cmd
@@ -1,1 +1,1 @@
-java -classpath demo-mpcontrol.jar;mpcontrol.jar -Djava.library.path=. demo.mpcontrol.CurrentSong
+java -classpath demo-mpcontrol.jar;jdic-mpcontrol.jar -Djava.library.path=. demo.mpcontrol.CurrentSong
