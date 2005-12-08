3e1d08207193ae40d8930ab9f08c711c3fe93cd2
zombi <zombi@ce359eda-5150-e5dd-abfe-a9d5ac1c7f9b>
2005-12-08 01:05:32 +0000

    Add demo flag to specify the path to the winamp.exe
    git-svn-id: https://svn.java.net/svn/jdic~svn/trunk@1282 ce359eda-5150-e5dd-abfe-a9d5ac1c7f9b

--- src/incubator/mpcontrol/demo/script/startplayers.cmd
+++ src/incubator/mpcontrol/demo/script/startplayers.cmd
@@ -1,2 +1,2 @@
-java -classpath demo-mpcontrol.jar;jdic-mpcontrol.jar -Djava.library.path=. demo.mpcontrol.StartPlayer
+java -classpath demo-mpcontrol.jar;jdic-mpcontrol.jar -Djava.library.path=. -Dorg.jdesktop.jdic.mpcontrol.winamp.path=c:\progs\winamp\winamp.exe demo.mpcontrol.StartPlayer
 
